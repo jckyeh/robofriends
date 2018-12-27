2c8adccc7daf884e8bbad9e45592f486bb550939
Jack Yeh <jackyeh@ip-192-168-2-126.ec2.internal>
2018-12-27 16:42:03 +0000

    styling to app, added componentDidMount method

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import { robots } from './robots';
+// import { robots } from './robots';
 import CardList from './CardList';
 import Searchbox from './Searchbox';
+import './app.css';
 
 class App extends React.Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
-        }
+        };
+        this.onSearchChange = this.onSearchChange.bind(this);
     }
+
+    componentDidMount() {
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => {this.setState({robots: users})})
+    }
+
+    onSearchChange(event) {
+        this.setState({ searchfield: event.target.value });
+    }
+
     render() {
-        return (
-            <div className="tc">
-                <h1>Robofriends</h1>
-                <Searchbox />
-                <CardList robots={ this.state.robots }/>
-            </div>
-        );
+        const filteredRobots = this.state.robots.filter((robot) => {
+            return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+        })
+        if (this.state.robots.length === 0) {
+            return <h1 className="tc">Loading</h1>
+        } else {
+            return (
+                <div className="tc">
+                    <h1 className="f1">Robofriends</h1>
+                    <Searchbox searchChange={ this.onSearchChange }/>
+                    <CardList robots={filteredRobots}/>
+                </div>
+            );
+        }
     }
 }
 
